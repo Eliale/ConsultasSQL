fb7ef4c6380c39b42445b06048a9f2171b00a0e4
Eli Alejandro <elialejandros@gmail.com>
2016-07-13 18:26:57 -0500

    Nuevas

--- consultasEli.sql
+++ consultasEli.sql
@@ -26,3 +26,30 @@ sum(case when c.fecha_nac between ((select current_date) - (111 * '1 year'::inte
 from cliente as c
 inner join bancouno."serviciosPrestamo"  as sp
 on c.id_cliente = sp.id_cliente
+
+--  Clientes que tienen cuentas de inversión pero no han pedido ningún préstamo. Listado y Totales. 
+select c.id_cliente,c.nombre,c.apellido_p,c.apellido_m from cliente as c inner join servicio_cuenta as sc
+on c.id_cliente=sc.id_cliente inner join servicio_inversion as si
+on sc.no_cuenta=si.no_cuentagral 
+EXCEPT 
+select c.id_cliente,c.nombre,c.apellido_p,c.apellido_m from cliente as c inner join "serviciosPrestamo" as sp
+on c.id_cliente = sp.id_cliente
+group by c.id_cliente
+
+-- 7.- Clientes que tienen cuentas de inversión, cuentas de ahorro pero no han pedido ningún préstamo.
+(select c.id_cliente,c.nombre,c.apellido_p,c.apellido_m from cliente as c inner join servicio_cuenta as sc
+on c.id_cliente=sc.id_cliente inner join servicio_inversion as si
+on sc.no_cuenta=si.no_cuentagral inner join servicio_ahorro as sa
+on sa.id_cliente=sc.id_cliente and sc.no_cuenta=si.no_cuentagral 
+EXCEPT 
+select c.id_cliente,c.nombre,c.apellido_p,c.apellido_m from cliente as c inner join "serviciosPrestamo" as sp
+on c.id_cliente = sp.id_cliente
+group by c.id_cliente)
+
+--8.- Clientes que pidieron un préstamo a menos de 3 meses de ingresar al banco. 
+select sp.id_cliente,c.nombre,c.apellido_p,c.apellido_m from cliente as c inner join 
+"serviciosPrestamo" as sp
+on c.id_cliente = sp.id_cliente inner join  servicio_cuenta as sc
+on sc.id_cliente = sp.id_cliente
+where sp.fecha_contratacion<(sc.fecha_contratacion + 90)
+group by sp.id_cliente,c.nombre,c.apellido_p,c.apellido_m 
